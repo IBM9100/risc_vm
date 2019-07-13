e27c0aa8d388e43f5a3976a44c88b284c536cc4e
IBM9100 <chuyuekayo@gmail.com>
2019-07-14 00:06:38 +0800

    add Main loop, op_add

---
+++ isa.c
@@ -0,0 +1,36 @@
+#include "common.h"
+#include "isa.h"
+#include "register.h"
+
+inline uint16_t sign_extend(uint16_t low, uint16_t len){
+    if(low >> (len - 1) & 1){
+        low |= (0xFFFF << len);
+    }
+    return low;
+}
+
+inline void update_flags(uint16_t rx){
+    if(reg[rx] == 0) reg[R_COND] = FL_ZRO;
+    else if(reg[rx] >> 15) reg[R_COND] = FL_NEG;
+    else reg[R_COND] = FL_POS;
+}
+
+/* 指令集实现 */
+inline void op_add(uint16_t instc){
+    /* 0x7: 通用寄存器掩码 */
+    /* destination register */
+    uint16_t r0 = (instc >> 9) & 0x7;
+    /* SR1 */
+    uint16_t r1 = (instc >> 6) & 0x7;
+    /* 0/SR2 1/imm5 */
+    uint16_t imm_flag = (instc >> 5) & 0x1;
+
+    if(imm_flag){
+        reg[r0] = reg[r1] + (sign_extend(instc & 0x1F, 5));
+    }else{
+        uint16_t r2 = instc & 0x7;
+        reg[r0] = reg[r1] + reg[r2];
+    }
+
+    update_flags(r0);
+}
---
+++ main.c
@@ -0,0 +1,22 @@
+#include "common.h"
+#include "memory.h"
+#include "register.h"
+
+/* 
+ *  1. 从 R_PC 读入指令
+ *  2. 递增 PC
+ *  3. 执行 opcode
+ *  4. 跳到 1
+ */
+int main(int argc, const char* argv[]){
+    
+    /* CPU加电稳定后执行的第一条指令 */
+    reg[R_PC] = PC_START;
+
+    int32_t running = 1;
+    
+    while(running){
+        uint16_t instrc = mem_read(reg[R_PC]++);
+        uint16_t op = instrc >> 12;
+    }
+}
