6c252171f9552f7eec0afba4415289afeeb78c93
IBM9100 <chuyuekayo@gmail.com>
2019-07-13 22:47:12 +0800

    add FLAGS, ISA

--- common.h
+++ common.h
@@ -2,3 +2,23 @@
 
 /* 标准库 */
 #include<stdint.h>
+
+/* ISA */
+enum {
+    OP_BR = 0, /* branch */
+    OP_ADD,    /* add  */
+    OP_LD,     /* load */
+    OP_ST,     /* store */
+    OP_JSR,    /* jump register */
+    OP_AND,    /* bitwise and */
+    OP_LDR,    /* load register */
+    OP_STR,    /* store register */
+    OP_RTI,    /* unused */
+    OP_NOT,    /* bitwise not */
+    OP_LDI,    /* load indirect */
+    OP_STI,    /* store indirect */
+    OP_JMP,    /* jump */
+    OP_RES,    /* reserved (unused) */
+    OP_LEA,    /* load effective address */
+    OP_TRAP    /* execute trap */
+};
--- register.h
+++ register.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "common.h"
 
 /* Register */
 enum{
@@ -9,8 +10,17 @@ enum{
     R_R4,
     R_R5,
     R_R6,
-    R_R7,       //8个通用寄存器
-    R_PC,       //两个专用寄存器
-    R_COND,     //EFLAGS
+    R_R7,       /* 8个通用寄存器 */
+    R_PC,       /* 两个专用寄存器 */
+    R_COND,     /* EFLAGS */
     R_COUNT
 };
+
+/* FLAGS */
+enum {
+    FL_POS = 1 << 0,    /* P */
+    FL_ZRO = 1 << 1,    /* Z */
+    FL_NEG = 1 << 2,    /* N */
+};
+
+uint16_t reg[R_COUNT];
